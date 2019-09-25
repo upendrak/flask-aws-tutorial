4d0aa22b0b501ec330308d6b25c90bfaab447688
upendrak <upendrakumar.devisetty@googlemail.com>
2019-09-24 20:52:34 -0700

    Change the url of the AWS RDS

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:flask123@flaskinsight.crrvqov9dgv2.us-east-2.rds.amazonaws.com:3306/BucketList'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -10,4 +10,5 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+# SECRET_KEY = 'NkPrgJxKQbBhYfGR5aH/x7YsXvkngVzvp47Qv7mP'
+SECRET_KEY = 'a2349870e1752ef40d76385184e78b47'
