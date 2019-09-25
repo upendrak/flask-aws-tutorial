2330f23df21ef816d7238419f3d3418d0a4951ba
upendrak <upendrakumar.devisetty@googlemail.com>
2019-09-24 20:52:06 -0700

    Change libraries

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 application.config.from_object('config')
